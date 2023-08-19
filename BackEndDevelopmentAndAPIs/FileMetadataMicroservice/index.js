5484e6978a4931f2c588d7366b76e306894703d9
Hackodes <36645426+Hackodes@users.noreply.github.com>
2023-08-19 19:42:05 +0100

    Update index.js

--- BackEndDevelopmentAndAPIs/FileMetadataMicroservice/index.js
+++ BackEndDevelopmentAndAPIs/FileMetadataMicroservice/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,10 +12,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
 
+  const fileInfo = {
+    name: originalname,
+    type: mimetype,
+    size: size
+  };
+
+  res.json(fileInfo);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
