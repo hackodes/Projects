5af9cf6a18abb2e84730b0fab817b84c4336cfb5
hackodes <36645426+hackodes@users.noreply.github.com>
2024-05-12 21:35:22 +0100

    Update tailwind.config.js

--- QualityAssurance/AmericanBritishTranslator/tailwind.config.js
+++ QualityAssurance/AmericanBritishTranslator/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./views/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: ['./views/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
