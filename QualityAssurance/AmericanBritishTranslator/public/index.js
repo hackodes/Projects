97dd5ba512b22c7ce86bf5d3fdfe9edec78b95c1
hackodes <36645426+hackodes@users.noreply.github.com>
2024-05-12 21:35:05 +0100

    Update index.js

--- QualityAssurance/AmericanBritishTranslator/public/index.js
+++ QualityAssurance/AmericanBritishTranslator/public/index.js
@@ -1,33 +1,34 @@
-
 const translateHandler = async () => {
-  const textArea = document.getElementById("text-input");
-  const localeArea = document.getElementById("locale-select");
-  const errorArea = document.getElementById("error-msg");
-  const translatedArea = document.getElementById("translated-sentence");
-  const solutionContainer = document.getElementById("solution-container");
+  const textArea = document.getElementById('text-input')
+  const localeArea = document.getElementById('locale-select')
+  const errorArea = document.getElementById('error-msg')
+  const translatedArea = document.getElementById('translated-sentence')
+  const solutionContainer = document.getElementById('solution-container')
 
-  solutionContainer.classList.remove("hidden");
-  
-  errorArea.innerText = "Error message will appear here...";
-  translatedArea.innerText = "Translated sentence will appear here...";
+  solutionContainer.classList.remove('hidden')
 
-  const data = await fetch("/api/translate", {
-    method: "POST",
+  errorArea.innerText = ''
+  translatedArea.innerText = ''
+
+  const data = await fetch('/api/translate', {
+    method: 'POST',
     headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
+      Accept: 'application/json',
+      'Content-type': 'application/json',
     },
-    body: JSON.stringify({"text": textArea.value, "locale": localeArea.value})
-  });
+    body: JSON.stringify({ text: textArea.value, locale: localeArea.value }),
+  })
 
-  const parsed = await data.json();
+  const parsed = await data.json()
   if (parsed.error) {
-    errorArea.innerText = JSON.stringify(parsed);
-    return;
+    translatedArea.style.display = 'none'
+    errorArea.innerText = JSON.stringify(parsed)
+    return
   }
 
-  translatedArea.innerHTML = parsed.translation;
-  return;
-};
+  errorArea.style.display = 'none'
+  translatedArea.innerHTML = parsed.translation
+  return
+}
 
-document.getElementById("translate-btn").addEventListener("click", translateHandler)
+document.getElementById('translate-btn').addEventListener('click', translateHandler)
