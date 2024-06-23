cb7ca64cd317c11866fec5719315b8e3d51a15ce
hackodes <36645426+hackodes@users.noreply.github.com>
2024-06-23 19:45:15 +0100

    Create bookModel.js

---
+++ QualityAssurance/PersonalLibrary/models/bookModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const BookSchema = new Schema({
+  title: { type: String, required: true },
+  comments: { type: [String], default: [] },
+})
+
+const Book = mongoose.model("Book", BookSchema)
+
+module.exports = Book
