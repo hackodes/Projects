8a370f5e9aaee4cfa7fb19651df5e8d60b16277d
hackodes <36645426+hackodes@users.noreply.github.com>
2024-11-14 23:18:34 +0000

    Create testEntitiesHelpers.js

---
+++ InformationSecurity/AnonymousMessageBoard/helpers/testEntitiesHelpers.js
@@ -0,0 +1,16 @@
+const test_thread_data = {
+    board: "anon-board",
+    text: "First thread text",
+    delete_password: "test_thread_password",
+  };
+  
+  const test_reply_data = {
+    text: "First reply text",
+    delete_password: "test_reply_password"
+  };
+  
+  module.exports = {
+    test_thread_data,
+    test_reply_data,
+  };
+  
